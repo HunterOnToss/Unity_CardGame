49f04583d99c114da579f89f5fdb920f734d4b8a
hunter <stasdolg@mail.ru>
2017-03-10 10:53:37 +0700

    make " to turn the cards over"

---
+++ Assets/_Scripts/MemoryCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+	[SerializeField] private GameObject cardBack;
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf) {
+			cardBack.SetActive (false);
+		}
+	}
+}
